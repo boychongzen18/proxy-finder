2f06bd316b98c58efa3379c850992f4d27314f25
boychongzen <38745550+boychongzen18@users.noreply.github.com>
2019-07-25 23:41:14 +0700

    Add files via upload

---
+++ proxy-finder.py
@@ -0,0 +1,21 @@
+import requests
+
+# Tools Priv8 Tools Proxy Finder
+# Output proxy.txt 
+# Tested : Windows & Termux Working
+# eMAIL  : hackingforlive@yahoo.com
+# Regard : Boychongzen aka Xroot
+
+
+print ('''\033[1m\033[93m[\033[91m!\033[93m] Tools Priv8 Proxy Finder [ SALE ] \033[1;37m
+Welcombek My Tools \n \033[1;31m
+     _____         _                             
+    |_   _|__  ___| |___                         
+  ___ | |/ _ \/ _ \ (_-<   ___ _         _         
+ | _ \|_|\___/\___/_/__/  | __(_)_ _  __| |___ _ _ 
+ |  _/ '_/ _ \ \ / || |   | _|| | ' \/ _` / -_) '_|
+ |_| |_| \___/_\_\\_, |    |_| |_|_||_\__,_\___|_|  
+                  |__/                                                         
+           \033[1;34mAuthor \033[91m:\033[37m Boychongzen aka Xroot                                  
+\033[1;34mMy \033[91m:\033[37mFacebook\033[91m:\033[37m https://web.facebook.com/xroot.xroot.7 > Xroot  \n \033[1;34m    
+ ''')
